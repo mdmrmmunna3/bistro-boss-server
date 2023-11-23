d570257ea30cf408900c5dac983cd82d21d3db6d
mdmrmmunna3 <mdmunnaking1209@gmail.com>
2023-11-23 21:54:18 +0600

    create delete api for users

--- index.js
+++ index.js
@@ -67,6 +67,14 @@ async function run() {
             res.send(result);
         })
 
+        // users delete apis
+        app.delete('/users/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) };
+            const result = await usersCollection.deleteOne(query);
+            res.send(result);
+        })
+
         // get menu 
         app.get('/menu', async (req, res) => {
             const result = await menuCollection.find().toArray();
