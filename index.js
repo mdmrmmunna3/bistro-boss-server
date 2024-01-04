12b498ed06cda921deecd6c487d5c53b6c0fcd7f
mdmrmmunna3 <mdmunnaking1209@gmail.com>
2024-01-04 21:49:57 +0600

    secure added and  save new menu item with verifyJwt and verifyAdmin middelware

--- index.js
+++ index.js
@@ -136,6 +136,13 @@ async function run() {
             res.send(result);
         })
 
+
+        app.post('/menu', verifyJWT, verifyAdmin, async (req, res) => {
+            const newItem = req.body;
+            const result = await menuCollection.insertOne(newItem)
+            res.send(result);
+        })
+
         // get reviews
         app.get('/reviews', async (req, res) => {
             const result = await reviewCollection.find().toArray();
