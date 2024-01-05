f793f7721f20a0937aca8e2704acf20be0fcc17e
mdmrmmunna3 <mdmunnaking1209@gmail.com>
2024-01-05 21:57:48 +0600

    created deleted menu item api with verifyjwt and verifyAdmin middleware on backend

--- index.js
+++ index.js
@@ -136,10 +136,18 @@ async function run() {
             res.send(result);
         })
 
-
+        // post new menu item
         app.post('/menu', verifyJWT, verifyAdmin, async (req, res) => {
             const newItem = req.body;
             const result = await menuCollection.insertOne(newItem)
+            res.send(result);
+        })
+
+        // deleted a menu item 
+        app.delete('/menu/:id', verifyJWT, verifyAdmin, async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) }
+            const result = await menuCollection.deleteOne(query);
             res.send(result);
         })
 
